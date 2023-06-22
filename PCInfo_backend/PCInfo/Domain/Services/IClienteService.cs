7d7fe4a2f6bd57344f002773972027e16444c0fc
Luis D. Maco <luisdmaco@outlook.com>
2023-06-22 18:27:07 -0500

    Service interfaces added

---
+++ PCInfo_backend/PCInfo/Domain/Services/IClienteService.cs
@@ -0,0 +1,12 @@
+﻿using PCInfo_backend.PCInfo.Domain.Models;
+using PCInfo_backend.PCInfo.Domain.Services.Communication;
+
+namespace PCInfo_backend.PCInfo.Domain.Services;
+
+public interface IClienteService
+{
+    Task<IEnumerable<Cliente>> ListAsync();
+    Task<ClienteResponse> SaveAsync(Cliente cliente);
+    Task<ClienteResponse> UpdateAsync(int id, Cliente cliente);
+    Task<ClienteResponse> DeleteAsync(int id);
+}
---
+++ PCInfo_backend/PCInfo/Domain/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using PCInfo_backend.PCInfo.Domain.Models;
+using PCInfo_backend.PCInfo.Domain.Services.Communication;
+
+namespace PCInfo_backend.PCInfo.Domain.Services;
+
+public interface IUserService
+{
+    Task<IEnumerable<User>> ListAsync();
+    Task<UserResponse> SaveAsync(User user);
+    Task<UserResponse> UpdateAsync(int id, User user);
+    Task<UserResponse> DeleteAsync(int id);
+}
