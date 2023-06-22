9f8aa87e00d309021d1a44b3c3a539f7003d30cd
Luis D. Maco <luisdmaco@outlook.com>
2023-06-22 18:06:21 -0500

    Models created

---
+++ PCInfo_backend/PCInfo/Domain/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿namespace PCInfo_backend.PCInfo.Domain.Models;
+
+public class Cliente
+{
+    public string DNI { get; set; }
+    public string Nombre { get; set; }
+    public string Apellidos { get; set; }
+    public string Telefono { get; set; }
+    public string Correo { get; set; }
+    public string Direccion { get; set; }
+    
+    //Relationships
+    public string IDuser { get; set; }
+    public User user { get; set; }
+}
---
+++ PCInfo_backend/PCInfo/Domain/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace PCInfo_backend.PCInfo.Domain.Models;
+
+public class User
+{
+    public string IDuser { get; set; }
+    public string Usuario { get; set; }
+    public string Password { get; set; }
+
+    //Relationships
+    public IList<Cliente> Clientes { get; set; } = new List<Cliente>();
+}
