d363b996045c0d3870f23fae3d32a1c8dc50c608
Luis D. Maco <luisdmaco@outlook.com>
2023-06-22 18:23:09 -0500

    Response classes added

---
+++ PCInfo_backend/PCInfo/Domain/Services/Communication/ClienteResponse.cs
@@ -0,0 +1,14 @@
+﻿using PCInfo_backend.PCInfo.Domain.Models;
+using PCInfo_backend.Shared.Domain.Services.Communication;
+
+namespace PCInfo_backend.PCInfo.Domain.Services.Communication;
+
+public class ClienteResponse : BaseResponse<Cliente>
+{
+    public ClienteResponse(string message) : base(message)
+    {
+    }
+    public ClienteResponse(Cliente resource) : base(resource)
+    {
+    }
+}
---
+++ PCInfo_backend/PCInfo/Domain/Services/Communication/UserResponse.cs
@@ -0,0 +1,14 @@
+﻿using PCInfo_backend.PCInfo.Domain.Models;
+using PCInfo_backend.Shared.Domain.Services.Communication;
+
+namespace PCInfo_backend.PCInfo.Domain.Services.Communication;
+
+public class UserResponse : BaseResponse<User>
+{
+    UserResponse(string message) : base(message)
+    {
+    }
+    UserResponse(User resource) : base(resource)
+    {
+    }
+}
