4d1ca1b5d8a947cf29c0636f80f3cc3fa71e9a20
Luis D. Maco <luisdmaco@outlook.com>
2023-06-22 18:17:32 -0500

    Repository interfaces added

---
+++ PCInfo_backend/PCInfo/Domain/Repositories/IClienteRepository.cs
@@ -0,0 +1,12 @@
+﻿using PCInfo_backend.PCInfo.Domain.Models;
+
+namespace PCInfo_backend.PCInfo.Domain.Repositories;
+
+public interface IClienteRepository
+{
+    Task<IEnumerable<Cliente>> ListAsync();
+    Task<Cliente> GetAsync(string id);
+    Task SaveAsync(Cliente cliente);
+    void UpdateAsync(string id, Cliente cliente);
+    void DeleteAsync(string id);
+}
---
+++ PCInfo_backend/PCInfo/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using PCInfo_backend.PCInfo.Domain.Models;
+
+namespace PCInfo_backend.PCInfo.Domain.Repositories;
+
+public interface IUserRepository
+{
+    Task<IEnumerable<User>> ListAsync();
+    Task<User> GetAsync(string id);
+    Task SaveAsync(User user);
+    void UpdateAsync(string id, User user);
+    void DeleteAsync(string id);
+}
