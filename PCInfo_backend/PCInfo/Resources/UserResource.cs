d25671a1260213f2f2d469047f0c6fceab3e62a6
Luis D. Maco <luisdmaco@outlook.com>
2023-06-22 18:32:59 -0500

    Resource classes added

---
+++ PCInfo_backend/PCInfo/Resources/UserResource.cs
@@ -0,0 +1,8 @@
+﻿namespace PCInfo_backend.PCInfo.Resources;
+
+public class UserResource
+{
+    public string IDuser { get; set; }
+    public string Usuario { get; set; }
+    public string Password { get; set; }
+}
