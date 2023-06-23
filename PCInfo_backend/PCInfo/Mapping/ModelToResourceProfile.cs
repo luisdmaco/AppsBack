addc235fd9b46f26a81ca1def954c4fc4950e543
Luis D. Maco <luisdmaco@outlook.com>
2023-06-22 20:29:40 -0500

    ModelToResourceProfile and ResourceToModelProfile added

---
+++ PCInfo_backend/PCInfo/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PCInfo_backend.PCInfo.Domain.Models;
+using PCInfo_backend.PCInfo.Resources;
+
+namespace PCInfo_backend.PCInfo.Mapping;
+
+public class ModelToResourceProfile : Profile
+{
+    public ModelToResourceProfile()
+    {
+        CreateMap<User, UserResource>();
+        CreateMap<Cliente, ClienteResource>();
+    }
+}
---
+++ PCInfo_backend/PCInfo/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PCInfo_backend.PCInfo.Domain.Models;
+using PCInfo_backend.PCInfo.Resources;
+
+namespace PCInfo_backend.PCInfo.Mapping;
+
+public class ResourceToModelProfile : Profile
+{
+    public ResourceToModelProfile()
+    {
+        CreateMap<SaveUserResource, User>();
+        CreateMap<SaveClienteResource, Cliente>();
+    }
+}
