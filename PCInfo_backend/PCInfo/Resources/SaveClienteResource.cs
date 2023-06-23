5b812ff51f751e0a77112d1103430f80a8c26c18
Luis D. Maco <luisdmaco@outlook.com>
2023-06-22 20:26:49 -0500

    SaveResource classes added

---
+++ PCInfo_backend/PCInfo/Resources/SaveClienteResource.cs
@@ -0,0 +1,12 @@
+﻿namespace PCInfo_backend.PCInfo.Resources;
+
+public class SaveClienteResource
+{
+    public string DNI { get; set; }
+    public string Nombre { get; set; }
+    public string Apellidos { get; set; }
+    public string Telefono { get; set; }
+    public string Correo { get; set; }
+    public string Direccion { get; set; }
+    public string idUser { get; set; }
+}
---
+++ PCInfo_backend/PCInfo/Resources/SaveUserResource.cs
@@ -0,0 +1,7 @@
+﻿namespace PCInfo_backend.PCInfo.Resources;
+
+public class SaveUserResource
+{
+    public string Usuario { get; set; }
+    public string Password { get; set; }
+}
