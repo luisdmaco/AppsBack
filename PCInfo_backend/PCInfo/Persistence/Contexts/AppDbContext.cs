c89dde3e1aead8cf2f5b2beca0ff857c365ebb58
Luis D. Maco <luisdmaco@outlook.com>
2023-06-22 20:54:07 -0500

    AppDbContext added

---
+++ PCInfo_backend/PCInfo/Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PCInfo_backend.PCInfo.Domain.Models;
+
+namespace PCInfo_backend.PCInfo.Persistence.Contexts;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Cliente> Clientes { get; set; }
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<User>().ToTable("users");
+        builder.Entity<User>().HasKey(p => p.idUser);
+        builder.Entity<User>().Property(p => p.idUser).IsRequired().ValueGeneratedOnAdd();
+        
+        builder.Entity<Cliente>().ToTable("clientes");
+        builder.Entity<Cliente>().HasKey(p => p.idCliente);
+        builder.Entity<Cliente>().Property(p => p.idCliente).IsRequired().ValueGeneratedOnAdd();
+        
+        //Relationships
+        builder.Entity<Cliente>()
+            .HasOne(p => p.user)
+            .WithOne(p => p.cliente)
+            .HasForeignKey<Cliente>(c => c.idUser);
+    }
+}
