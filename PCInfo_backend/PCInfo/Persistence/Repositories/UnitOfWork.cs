8370b7c2e07414ab1f2de41c036e8688395254d4
Luis D. Maco <luisdmaco@outlook.com>
2023-06-22 21:05:23 -0500

    UnitOfWork added

---
+++ PCInfo_backend/PCInfo/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using PCInfo_backend.PCInfo.Domain.Repositories;
+using PCInfo_backend.PCInfo.Persistence.Contexts;
+
+namespace PCInfo_backend.PCInfo.Persistence.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+    
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task CompleteAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
