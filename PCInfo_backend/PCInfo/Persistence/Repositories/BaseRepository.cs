877a0a9019808bcdbe2220451873d693dbfa4179
Luis D. Maco <luisdmaco@outlook.com>
2023-06-22 20:54:17 -0500

    BaseRepository generated

---
+++ PCInfo_backend/PCInfo/Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using PCInfo_backend.PCInfo.Persistence.Contexts;
+
+namespace PCInfo_backend.PCInfo.Persistence.Repositories;
+
+public class BaseRepository
+{
+    protected readonly AppDbContext _context;
+    
+    public BaseRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+}
