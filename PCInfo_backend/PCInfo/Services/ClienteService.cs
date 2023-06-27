e757da026a90b1454d7b616fa1f03874e53de91c
Luis D. Maco <luisdmaco@outlook.com>
2023-06-27 16:53:43 -0500

    Service classes added

---
+++ PCInfo_backend/PCInfo/Services/ClienteService.cs
@@ -0,0 +1,48 @@
+﻿using PCInfo_backend.PCInfo.Domain.Models;
+using PCInfo_backend.PCInfo.Domain.Repositories;
+using PCInfo_backend.PCInfo.Domain.Services;
+using PCInfo_backend.PCInfo.Domain.Services.Communication;
+
+namespace PCInfo_backend.PCInfo.Services;
+
+public class ClienteService : IClienteService
+{
+    private readonly IClienteRepository _clienteRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    
+    public ClienteService(IClienteRepository clienteRepository, IUnitOfWork unitOfWork)
+    {
+        _clienteRepository = clienteRepository;
+        _unitOfWork = unitOfWork;
+    }
+    
+    public async Task<IEnumerable<Cliente>> ListAsync()
+    {
+        return await _clienteRepository.ListAsync();
+    }
+
+    public async Task<ClienteResponse> SaveAsync(Cliente cliente)
+    {
+        try
+        {
+            await _clienteRepository.AddAsync(cliente);
+            await _unitOfWork.CompleteAsync();
+            return new ClienteResponse(cliente);
+        }
+        catch (Exception e)
+        {
+            return new ClienteResponse($"An error occurred while saving the cliente: {e.Message}");
+        }
+        
+    }
+
+    public Task<ClienteResponse> UpdateAsync(int id, Cliente cliente)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ClienteResponse> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ PCInfo_backend/PCInfo/Services/UserService.cs
@@ -0,0 +1,47 @@
+﻿using PCInfo_backend.PCInfo.Domain.Models;
+using PCInfo_backend.PCInfo.Domain.Repositories;
+using PCInfo_backend.PCInfo.Domain.Services;
+using PCInfo_backend.PCInfo.Domain.Services.Communication;
+
+namespace PCInfo_backend.PCInfo.Services;
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UserService(IUserRepository userRepository, IUnitOfWork unitOfWork)
+    {
+        _userRepository = userRepository;
+        _unitOfWork = unitOfWork;
+    }
+    
+    public async Task<IEnumerable<User>> ListAsync()
+    {
+        return await _userRepository.ListAsync();
+    }
+
+    public async Task<UserResponse> SaveAsync(User user)
+    {
+        try
+        {
+            await _userRepository.SaveAsync(user);
+            await _unitOfWork.CompleteAsync();
+            return new UserResponse(user);
+        }
+        catch (Exception e)
+        {
+            return new UserResponse($"An error occurred while saving the user: {e.Message}");
+        }
+    }
+
+    public Task<UserResponse> UpdateAsync(int id, User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserResponse> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
