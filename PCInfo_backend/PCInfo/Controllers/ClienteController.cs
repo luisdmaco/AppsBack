b41f8ba9efa54a6afd433b802688d9be4216595d
Luis D. Maco <luisdmaco@outlook.com>
2023-06-28 01:02:06 -0500

    Controller classes added

---
+++ PCInfo_backend/PCInfo/Controllers/ClienteController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PCInfo_backend.PCInfo.Domain.Models;
+using PCInfo_backend.PCInfo.Domain.Services;
+using PCInfo_backend.PCInfo.Resources;
+using PCInfo_backend.PCInfo.Services;
+using PCInfo_backend.Shared.Extensions;
+
+namespace PCInfo_backend.PCInfo.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class ClienteController : ControllerBase
+{
+    private readonly ClienteService _clienteService;
+    private readonly IMapper _mapper;
+    
+    public ClienteController(ClienteService clienteService, IMapper mapper)
+    {
+        _clienteService = clienteService;
+        _mapper = mapper;
+    }
+    
+    [HttpGet]
+    public async Task<IEnumerable<ClienteResource>> GetAllAsync()
+    {
+        var clients = await _clienteService.ListAsync();
+        var resources = _mapper.Map<IEnumerable<Cliente>, IEnumerable<ClienteResource>>(clients);
+
+        return resources;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] SaveClienteResource resource)
+    {
+        if(!ModelState.IsValid)
+            return BadRequest(ModelState.GetErrorMessages());
+        var cliente = _mapper.Map<SaveClienteResource, Cliente>(resource);
+        var result = await _clienteService.SaveAsync(cliente);
+        if (!result.Success)
+            return BadRequest(result.Message);
+        var clienteResource = _mapper.Map<Cliente, ClienteResource>(result.Resource);
+        return Ok(clienteResource);
+    }
+}
---
+++ PCInfo_backend/PCInfo/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PCInfo_backend.PCInfo.Domain.Models;
+using PCInfo_backend.PCInfo.Domain.Services;
+using PCInfo_backend.PCInfo.Resources;
+using PCInfo_backend.PCInfo.Services;
+using PCInfo_backend.Shared.Extensions;
+
+namespace PCInfo_backend.PCInfo.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+    private readonly IMapper _mapper;
+    
+    public UserController(IUserService userService, IMapper mapper)
+    {
+        _userService = userService;
+        _mapper = mapper;
+    }
+    
+    [HttpGet]
+    public async Task<IEnumerable<UserResource>> GetAllAsync()
+    {
+        var users = await _userService.ListAsync();
+        var resources = _mapper.Map<IEnumerable<User>, IEnumerable<UserResource>>(users);
+
+        return resources;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] SaveUserResource resource)
+    {
+        if(!ModelState.IsValid)
+            return BadRequest(ModelState.GetErrorMessages());
+        var category = _mapper.Map<SaveUserResource, User>(resource);
+        var result = await _userService.SaveAsync(category);
+        if (!result.Success)
+            return BadRequest(result.Message);
+        var userResource = _mapper.Map<User, UserService>(result.Resource);
+        return Ok(userResource);
+    }
+}
