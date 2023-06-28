67671af90729d5bbd1739608c07b719dcb8956ba
Luis D. Maco <luisdmaco@outlook.com>
2023-06-28 01:02:50 -0500

    Program hotfix

--- PCInfo_backend/Program.cs
+++ PCInfo_backend/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using PCInfo_backend.PCInfo.Domain.Repositories;
+using PCInfo_backend.PCInfo.Domain.Services;
+using PCInfo_backend.PCInfo.Mapping;
+using PCInfo_backend.PCInfo.Persistence.Contexts;
+using PCInfo_backend.PCInfo.Persistence.Repositories;
+using PCInfo_backend.PCInfo.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +15,45 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add Database Connection
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySQL(connectionString)
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors());
+
+// Add lowercase routes
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// Dependency Injection Configuration
+
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+builder.Services.AddScoped<IClienteService, ClienteService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddScoped<ClienteService>();
+
+// AutoMapper Configuration
+
+builder.Services.AddAutoMapper(
+    typeof(ModelToResourceProfile),
+    typeof(ResourceToModelProfile));
+
 var app = builder.Build();
+
+// Validation for ensuring Database Objects are created
+
+using (var scope = app.Services.CreateScope())
+using (var context = scope.ServiceProvider.GetService<AppDbContext>())
+{
+        context.Database.EnsureCreated(); 
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
